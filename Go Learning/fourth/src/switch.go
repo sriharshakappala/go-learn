a38e6d04ccb45941d69443172b3a8560415c9975
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-06-14 12:40:42 +0530

    Switch in go lang

---
+++ Go Learning/fourth/src/switch.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Enter a number: ")
+  var i float64
+  fmt.Scanf("%f", &i)
+  switch i {
+    case 0: fmt.Println("Zero")
+    case 1: fmt.Println("One")
+    case 2: fmt.Println("Two")
+    case 3: fmt.Println("Three")
+    case 4: fmt.Println("Four")
+    case 5: fmt.Println("Five")
+    default: fmt.Println("Unknown")
+  }
+}
