b7659de80cd679d140b74715fc4db4e51d6797fe
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-11-01 23:02:17 +0530

    Added delay in goroutine calls

--- Go Learning/go_routine1.go
+++ Go Learning/go_routine1.go
@@ -2,11 +2,15 @@ package main
 
 import (
   "fmt"
+  "math/rand"
+  "time"
 )
 
 func f(n int) {
   for i := 0; i < 1000; i++ {
     fmt.Println(n, ":", i)
+    amt := time.Duration(rand.Intn(250))
+    time.Sleep(time.Millisecond * amt)
   }
 }
 
